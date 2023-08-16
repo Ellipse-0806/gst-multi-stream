c66b4b6246208c2ca9c00a29efcb1adbfa30289e
r-kato <r-kato@musen.co.jp>
2023-08-16 18:00:14 +0900

    サンプル動画で、設定ファイルの情報を元にストリームを表示できるようにコードを作成

--- stream_provider/src/main.rs
+++ stream_provider/src/main.rs
@@ -1,7 +1,29 @@
 use config_parser;
+use gst_utils::pipeline::comp::MultiStreamPL;
+use gst_utils::pipeline::sink::SinkElement;
+use gst_utils::pipeline::src::uri::SourcePad;
+use gst_utils::pipeline::Pipeline;
 
 fn main() {
-    config_parser::load();
-    // let num = config_parser::add(11, 10);
-    // println!("Hello, world! {}", num);
+    let config = config_parser::load().expect("Could not loading setting file.");
+    let sources = config.get_sources();
+    let pipeline = Pipeline::new().unwrap();
+    let multi_stream = MultiStreamPL::new(pipeline.get_pipeline()).unwrap();
+    let sink = SinkElement::new(pipeline.get_pipeline()).unwrap();
+    multi_stream.link(sink.get_element()).unwrap();
+
+    for (i, source) in sources.iter().enumerate() {
+        println!("No.{}\n{:#?}", i, source);
+        let src_pad = SourcePad::new(source.get_uri(), pipeline.get_pipeline(), i).unwrap();
+        multi_stream
+            .link_pad(
+                source.get_x_pos(),
+                source.get_y_pos(),
+                source.get_width(),
+                source.get_height(),
+                src_pad.get_src_pad(),
+            )
+            .unwrap();
+    }
+    pipeline.run().unwrap();
 }
